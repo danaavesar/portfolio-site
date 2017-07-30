d335578154ada695a528c3ac0917710e66773695
danaavesar <danaavesar@gmail.com>
2017-07-30 15:03:41 +0300

    cideo width

--- js/main.js
+++ js/main.js
@@ -26,15 +26,10 @@ app.main = (function(mfaProjects){
 		console.log('Attaching events.');
 		var mainPage = true;
 		$(document).ready(function(){
-<<<<<<< HEAD
 			//---next and previous arrows---//
 			$('#descr-container').off('click').on('click', '.nav-arrow', function(){
 				console.log('next/prev click')
-=======
-			$('#descr-container').off('click').on('click', '.nav-arrow', function(){
->>>>>>> 44dc1af050846ca443f43e470037f5f165623cba
 				var currentProject;
-				console.log('drewwwwww')
 				$('.project-description').each(function(){
 					if($(this).is(":visible")){
 						currentProject = "#"+$(this).attr('description-name');
@@ -58,7 +53,6 @@ app.main = (function(mfaProjects){
 					prev.trigger('click');
 				}
 			})
-<<<<<<< HEAD
 			//---swipe to previous and next---//
 			$( "#parallax" ).off('swipeleft').on( "swipeleft", function(){
 				if(mainPage == false){
@@ -76,14 +70,6 @@ app.main = (function(mfaProjects){
 
 			})
 			//---click on project---//
-=======
-
-			// $('#descr-container').off('click').on('click', '#close-project',function(){
-			// 	$('.category-section').fadeIn();
-			//  	$('#descr-container').hide();
-			//  	mainPage = true;
-			// })
->>>>>>> 44dc1af050846ca443f43e470037f5f165623cba
 			$('.project-section').off('click').on('click', function(){
 			 	$('.category-section').hide();
 			 	var whichProject = $(this).attr('name');
@@ -269,23 +255,8 @@ app.main = (function(mfaProjects){
 				$("#mobile-bar").hide();
 			}
 		})
-<<<<<<< HEAD
 
 		//---scroll events---//
-=======
-		if($(window).width() < 480 && $('#parallax').scrollTop() < 1550){
-			// $( "#parallax" ).on( "scrollstart", function( event ) {
-			// 	console.log('scroll') 
-			// 	$('#parallax').animate({scrolltop: '1579px'}, 5000) ;
-			//  } )
-			if(scroll == true){
-				goToContent();
-			}
-			
-			
-					
-		}
->>>>>>> 44dc1af050846ca443f43e470037f5f165623cba
 		$('#parallax').scroll(function(){
 			console.log('parallax ' + $('#parallax').scrollTop())
 
@@ -331,7 +302,6 @@ app.main = (function(mfaProjects){
 	// 	$('.category-section').hide();
 	// 		 	var url = window.location.href;
 
-<<<<<<< HEAD
 	// 		 	var whichProject = url.substr(url.indexOf('?') + 1, url.length);
 
 	// 		 	$('#descr-container').fadeIn();
@@ -445,17 +415,6 @@ app.main = (function(mfaProjects){
 	// 	}, 700, 'easeOutQuart');
 	// 	scroll = false;
 	// }
-=======
-	
-	}
-	var goToContent = function(){
-			console.log('goToContent')
-		 $('#parallax').animate({
-		        		scrollTop: 1579 
-		    		}, 700, 'easeOutQuart');
-		 scroll = false;
-	}
->>>>>>> 44dc1af050846ca443f43e470037f5f165623cba
 	var render = function(viewName, data){
 		console.log('Rendering template for ' + viewName);
 		console.log('Received data: ' + data);
@@ -475,7 +434,6 @@ app.main = (function(mfaProjects){
 			$('#fake-menu').html(compiled(data));
 	
 		}else if(viewName == "descr"){
-<<<<<<< HEAD
 			
 			if($(window).width() <= 480){
 				$('body').append("<div id='nav-bar'><div class='nav-btns'><p class='nav-arrow' id='prev'>< Prev </p><div id='close-project'><span class='close-about-me fat heavy'></span></div><p class='nav-arrow' id='next'>Next ></p></div></div>");
@@ -483,9 +441,6 @@ app.main = (function(mfaProjects){
 				$('body').append("<div id='nav-bar'><div class='nav-btns'><p class='nav-arrow' id='prev'>< Prev </p><p class='nav-arrow' id='next'>Next ></p></div><div id='close-project'><span class='close-about-me fat heavy'></span></div></div>");
 			}
 			// $('#descr-container').append("<div id='nav-bar'><div class='nav-btns'><p class='nav-arrow' id='prev'>< Prev </p><p class='nav-arrow' id='next'>Next ></p></div><div id='close-project'><span class='close-about-me fat heavy'></span></div></div>");
-=======
-			$('#descr-container').append("<div id='nav-bar'><div class='nav-btns'><p class='nav-arrow' id='prev'>< Prev </p><p class='nav-arrow' id='next'>Next ></p></div><div id='close-project'><span class='close-about-me fat heavy'></span></div></div>");
->>>>>>> 44dc1af050846ca443f43e470037f5f165623cba
 			$('#descr-container').append(compiled(data));
 		}
 		
